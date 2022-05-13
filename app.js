8114552893b962c1d7fa955fe9905e169c38c869
Joren Bagadiong <joren.bagadiong14@gmail.com>
2022-05-13 22:59:18 +0800

    update loading of banks add stable

--- app.js
+++ app.js
@@ -1,3 +1,82 @@
+const INR_BANKS = [
+  "axis",
+  "brda",
+  "canbk",
+  "hdfc",
+  "icici",
+  "idbi",
+  "indu",
+  "kotak",
+  "kotbk",
+  "pnjb",
+  "sbi",
+  "yesb",
+];
+
+const MYR_BANKS = [
+  "my_amb",
+  "my_cimb",
+  "my_hlb",
+  "my_hsbc",
+  "my_mayb",
+  "my_ocbc",
+  "my_public",
+  "my_rhb",
+  "my_uobib",
+];
+
+const THB_BANKS = [
+  "th_bangkok",
+  "th_governmentsavings",
+  "th_kasikorn",
+  "th_krungthai",
+  "th_krungsri",
+  "th_siam",
+  "th_ttbdirect",
+  "th_uobt",
+];
+
+const VND_BANKS = [
+  "vn_acb",
+  "vn_bidv",
+  "vn_donga",
+  "vn_exim",
+  "vn_sacom",
+  "vn_techcom",
+  "vn_tp",
+  "vn_vietcom",
+  "vn_vietin",
+  "vn_vp",
+];
+
+const ZAR_BANKS = ["za_absa", "za_capitec", "za_fnb", "za_standard"];
+
+$(document).ready(function () {
+  loadBanks();
+});
+
+function loadBanks() {
+  INR_BANKS.map((bank) => {
+    $("#inrBank").append(`<option value="${bank}">${bank}</option>`);
+  });
+
+  MYR_BANKS.map((bank) => {
+    $("#myrBank").append(`<option value="${bank}">${bank}</option>`);
+  });
+
+  THB_BANKS.map((bank) => {
+    $("#thbBank").append(`<option value="${bank}">${bank}</option>`);
+  });
+
+  VND_BANKS.map((bank) => {
+    $("#vndBank").append(`<option value="${bank}">${bank}</option>`);
+  });
+
+  ZAR_BANKS.map((bank) => {
+    $("#zarBank").append(`<option value="${bank}">${bank}</option>`);
+  });
+}
+
 $("#host").change(function () {
   $(".banks").val("");
 });
@@ -36,6 +115,7 @@ function getPaymentReference(currency, bank) {
   const host = $("#host").val();
   const date = new Date();
   const paymentReference = date.getTime();
+  let url;
 
   if (!host) {
     alert("PLEASE CHOOSE HOST");
@@ -43,7 +123,7 @@ function getPaymentReference(currency, bank) {
   }
 
   if (host === "local") {
-    const localUrl =
+    url =
       "http://localhost:3050/start/" +
       bank +
       "?amount=100&accountNumber=mock&paymentReference=" +
@@ -51,12 +131,10 @@ function getPaymentReference(currency, bank) {
       "&returnUrl=https://www.google.com&accountName=someName&currency=" +
       currency +
       "&merchantCode=mc&expiryTime=2023-01-01T00:00:00.000Z&payOrderId=123422";
-
-    window.open(localUrl, "_blank");
   }
 
   if (host === "dev") {
-    const devUrl =
+    url =
       "https://puppeteer.dev.carpentum.tech/start/" +
       bank +
       "?amount=100&accountNumber=mock&paymentReference=" +
@@ -64,9 +142,19 @@ function getPaymentReference(currency, bank) {
       "&returnUrl=https://www.google.com&accountName=someName&currency=" +
       currency +
       "&merchantCode=mc&expiryTime=2023-01-01T00:00:00.000Z&payOrderId=55669988";
-
-    window.open(devUrl, "_blank");
   }
 
+  if (host === "stable") {
+    url =
+      "https://puppeteer.stable.carpentum.tech/start/" +
+      bank +
+      "?amount=100&accountNumber=mock&paymentReference=" +
+      paymentReference +
+      "&returnUrl=https://www.google.com&accountName=someName&currency=" +
+      currency +
+      "&merchantCode=mc&expiryTime=2023-01-01T00:00:00.000Z&payOrderId=55669988";
+  }
+
+  window.open(url, "_blank");
   $(".banks").val("");
 }
